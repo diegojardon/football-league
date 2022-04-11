23be89ac31c77c06cf8dc7921c0e8761b2db3811
Diego Jardon <dajr180787@gmail.com>
2022-04-10 20:50:35 -0500

    Add initial project configuration and dependencies

---
+++ services/src/main/java/mx/com/league/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package mx.com.league.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
